c3930dd36295271ad2a42ada76c28809208946a6
srahmed <srehanpk@gmail.com>
2020-06-02 16:15:14 +0500

    Update App.js
    lnew line remove

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World 
-          My first react app
+          
         </p>
         <a
           className="App-link"
