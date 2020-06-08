82ba5f137e1e8428f5dcb48081597a2952abe943
srahmed <srehanpk@gmail.com>
2020-06-08 18:03:02 +0500

    update css

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import List from './List.js';
 
 ReactDOM.render(
-  <>
+  <div className="main">
     <App name = "Rayhan Ahmed"/>
     <List />
-</>
+</div>
   ,
   document.getElementById('root')
 );
